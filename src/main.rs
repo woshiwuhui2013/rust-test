22ee5ab759deb14d5bed70115ee0607c879c9ddf
wuhui <56593113@qq.com>
2023-08-23 23:33:25 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use mylib::hello;
+// use std::net::TcpListener;
+fn main() {
+    let aa = hello();
+    println!("{}", aa)
+    // let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+}
---
+++ target/debug/deps/smawk-dee184f887d2c66a.d
@@ -0,0 +1,8 @@
+/Users/wuhui/IdeaProjects/current-work/harvey-sample/harveySample/rust-test/hello-rust/target/debug/deps/smawk-dee184f887d2c66a.rmeta: /Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/lib.rs /Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/monge.rs
+
+/Users/wuhui/IdeaProjects/current-work/harvey-sample/harveySample/rust-test/hello-rust/target/debug/deps/libsmawk-dee184f887d2c66a.rlib: /Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/lib.rs /Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/monge.rs
+
+/Users/wuhui/IdeaProjects/current-work/harvey-sample/harveySample/rust-test/hello-rust/target/debug/deps/smawk-dee184f887d2c66a.d: /Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/lib.rs /Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/monge.rs
+
+/Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/lib.rs:
+/Users/wuhui/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/smawk-0.3.1/src/monge.rs:
