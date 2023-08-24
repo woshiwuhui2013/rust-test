6be23cc1099069db86585f7fb182f92ccf118637
wuhui <56593113@qq.com>
2023-08-24 22:21:19 +0800

    net

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use mylib::hello;
-// use std::net::TcpListener;
-fn main() {
-    let aa = hello();
-    println!("{}", aa)
-    // let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for _stream in listener.incoming() {
+        let stream = _stream.unwrap();
+        println!("Connection established!");
+
+        handleStream(stream);
+
+    }
+}
+
+fn handleStream(mut stream: TcpStream){
+
+    let mut buf  = [0;512];
+
+    stream.read(&mut buf);
+
+    println!("{}", String::from_utf8_lossy(&buf));
 }
