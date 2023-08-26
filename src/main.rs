c309354ec465d630dc3daee53cee947be3d95c5c
wuhui <56593113@qq.com>
2023-08-26 22:48:12 +0800

    add thread

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,24 @@ fn handleStream(mut stream: TcpStream){
     let mut buf  = [0;512];
 
     stream.read(&mut buf);
+    println!("{}", String::from_utf8_lossy(&buf));
 
-    let get = b"GET/HTTP/1.1\r\n";
-    if buf.starts_with(get) {
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status, filename) = if buf.starts_with(get) {
         println!("success get");
-    }
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    }else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    println!("{}{}", filename, status );
 
     // println!("{}", String::from_utf8_lossy(&buf));
-    let content = fs::read_to_string("./index.html").unwrap();
+    let content = fs::read_to_string(filename).unwrap();
+    println!("file content {}", content);
 
-    let respond = format!("HTTP/1.1 200 OK \r\n\r\n{}", content);
+    let respond = format!("{}Content-Type: text/html; charset=utf-8\r\n{}", status, content);
+    println!("rsp {}", respond);
     stream.write(respond.as_bytes()).unwrap();
     stream.flush().unwrap();
 
